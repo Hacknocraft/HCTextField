aa9073af157847577ff0978e179bda22d64e6bbd
wanghao <hao.wang@hacknocraft.com>
2017-05-04 11:52:50 -0400

    initial commit

---
+++ HCTextField/Source/HCTextField.swift
@@ -0,0 +1,12 @@
+//
+//  HCTextField.swift
+//  HCTextField
+//
+//  Created by HAO WANG on 5/4/17.
+//  Copyright © 2017 Hacknocraft. All rights reserved.
+//
+
+import UIKit
+
+class HCTextField: UITextField {
+}
