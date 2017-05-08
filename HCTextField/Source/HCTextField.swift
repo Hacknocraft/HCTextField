3d4cf8924ff74f642fc1a8564c07d27a57c11bf5
Lebron <zhi719370309@live.com>
2017-05-08 22:26:34 +0800

    create HCTextField and UI

--- HCTextField/Source/HCTextField.swift
+++ HCTextField/Source/HCTextField.swift
@@ -7,7 +7,169 @@
 //
 
 import UIKit
+import Foundation
 
-class HCTextField: UITextField {
+private let kMinLength = 8
+private let kMaxLength = 16
+
+private enum HCTextFieldType {
+    case normal
+    case highlighted
+    case error
+}
+
+public struct HCTextFieldBorderColor {
+    static var normal: UIColor = .gray
+    static var highlighted: UIColor = .orange
+    static var error: UIColor = .red
+}
+
+public struct HCTextFieldCheckType: OptionSet {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    /// don't perform any check
+    static let none = HCTextFieldCheckType(rawValue: 1 << 0)
+    /// check if input is valid email
+    static let email = HCTextFieldCheckType(rawValue: 1 << 1)
+    /// check content length
+    static let length = HCTextFieldCheckType(rawValue: 1 << 2)
+    /// check if a put was missing
+    static let empty = HCTextFieldCheckType(rawValue: 1 << 3)
+}
+
+open class HCTextField: UITextField {
+
+    open var checkType: HCTextFieldCheckType = .empty
+    open var errorMessage: String?
+    open var minLength: Int = kMinLength
+    open var maxLength: Int = kMaxLength
+    open var passedCheck: Bool = false
+    weak open var textFieldDelegate: HCTextFieldDelegate?
+
+    // MARK: - Initializers & setup data
+
+    public init(frame: CGRect,
+                checkType: HCTextFieldCheckType,
+                errorMessage: String?,
+                minLength: Int,
+                maxLength: Int,
+                placeholder: String?) {
+        self.checkType = checkType
+        self.errorMessage = errorMessage
+        self.minLength = minLength
+        self.maxLength = maxLength
+        super.init(frame: frame)
+        self.placeholder = placeholder
+    }
+
+    public init(frame: CGRect,
+                checkType: HCTextFieldCheckType,
+                errorMessage: String?,
+                placeholder: String?) {
+        self.checkType = checkType
+        self.errorMessage = errorMessage
+        super.init(frame: frame)
+        self.placeholder = placeholder
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    open func setCheckType(_ checkType: HCTextFieldCheckType,
+                           errorMessage: String?,
+                           minLength: Int = kMinLength,
+                           maxLength: Int = kMaxLength) {
+        self.checkType = checkType
+        self.errorMessage = errorMessage
+        self.minLength = minLength
+        self.maxLength = maxLength
+    }
+
+    // MARK: - Override methods
+
+    open override func becomeFirstResponder() -> Bool {
+        setBorder(for: .highlighted)
+        return super.becomeFirstResponder()
+    }
+
+    open override func resignFirstResponder() -> Bool {
+
+        if checkType.contains(.email) && !isEmail(text) {
+
+            passedCheck = false
+            setBorder(for: .error)
+            textFieldDelegate?.textField(self, didCheckFor: .email, errorMessage: errorMessage)
+
+        } else if checkType.contains(.length) && !satisfiesLengthRequirement(text) {
+
+            passedCheck = false
+            setBorder(for: .error)
+            textFieldDelegate?.textField(self, didCheckFor: .length, errorMessage: errorMessage)
+
+        } else if checkType.contains(.empty) && isEmpty(text) {
+
+            passedCheck = false
+            setBorder(for: .error)
+            textFieldDelegate?.textField(self, didCheckFor: .empty, errorMessage: errorMessage)
+
+        } else {
+
+            passedCheck = true
+            setBorder(for: .normal)
+            textFieldDelegate?.textField(self, didCheckFor: .none, errorMessage: nil)
+        }
+
+        return super.resignFirstResponder()
+    }
+
+    // MARK: - Private implementations
+
+    private func isEmail(_ text: String?) -> Bool {
+        guard let email = text else { return false }
+
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with:email)
+    }
+
+    private func satisfiesLengthRequirement(_ text: String?) -> Bool {
+        guard let content = text else { return false }
+
+        return (content.characters.count >= minLength) && (content.characters.count <= maxLength)
+    }
+
+    private func isEmpty(_ text: String?) -> Bool {
+        guard let t = text else { return false }
+        return t.isEmpty
+    }
+
+    private func setBorder(for type: HCTextFieldType) {
+        layer.cornerRadius = 3
+        switch type {
+        case .normal:
+            layer.borderWidth = 1
+            layer.borderColor = HCTextFieldBorderColor.normal.cgColor
+
+        case .highlighted:
+            layer.borderWidth = 1
+            layer.borderColor = HCTextFieldBorderColor.highlighted.cgColor
+
+        case .error:
+            layer.borderWidth = 1
+            layer.borderColor = HCTextFieldBorderColor.error.cgColor
+        }
+    }
+}
+
+// MARK: - HCTextFieldDelegate
+public protocol HCTextFieldDelegate: UITextFieldDelegate {
+
+    // Obejctive-C don's support OptionSet
+    func textField(_ textField: HCTextField, didCheckFor type: HCTextFieldCheckType, errorMessage: String?)
 
 }
--- HCTextField/ViewController.swift
+++ HCTextField/ViewController.swift
@@ -8,16 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, HCTextFieldDelegate {
+
+    @IBOutlet weak var nameField: HCTextField!
+    @IBOutlet weak var emailField: HCTextField!
+    @IBOutlet weak var passwordField: HCTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        nameField.setCheckType(.empty, errorMessage: "cann't be empty")
+        nameField.textFieldDelegate = self
+
+        emailField.setCheckType(.email, errorMessage: "not a valid email")
+        emailField.textFieldDelegate = self
+
+        passwordField.setCheckType(.length, errorMessage: "6 ~ 20 characters", minLength: 6, maxLength: 20)
+        passwordField.textFieldDelegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Actions
+
+    @IBAction func loginButtonTapped() {
+        _ = passwordField.resignFirstResponder()
+
+        var title = ""
+        var message: String?
+
+        if nameField.passedCheck && emailField.passedCheck && passwordField.passedCheck {
+            title = "Login Successfully"
+            message = nil
+        } else {
+            title = "Error"
+            message = "Please fill in the fields as required"
+        }
+
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default) { (_) in
+            alertVC.dismiss(animated: true, completion: nil)
+        }
+        alertVC.addAction(ok)
+
+        present(alertVC, animated: true, completion: nil)
+    }
+
+    // MARK: - HCTextFieldDelegate
+
+    func textField(_ textField: HCTextField, didCheckFor type: HCTextFieldCheckType, errorMessage: String?) {
+
+        switch type {
+        case HCTextFieldCheckType.email:
+            setRightView(for: textField, type: .email, errorMessage: errorMessage)
+
+        case HCTextFieldCheckType.empty:
+            setRightView(for: textField, type: .empty, errorMessage: errorMessage)
+
+        case HCTextFieldCheckType.length:
+            setRightView(for: textField, type: .length, errorMessage: errorMessage)
+
+        case HCTextFieldCheckType.none:
+            setRightView(for: textField, type: .none, errorMessage: errorMessage)
+
+        default:
+            break
+        }
+    }
+
+    // MARK: - Private implementations
+
+    private func setRightView(for textField: HCTextField, type: HCTextFieldCheckType, errorMessage: String?) {
+        guard let message = errorMessage else {
+            textField.rightViewMode = .never
+            return
+        }
+
+        if type.contains(.none) {
+            textField.rightViewMode = .never
+        } else {
+            textField.rightViewMode = .unlessEditing
+        }
+
+        let attrText = NSMutableAttributedString(string: message,
+                                                 attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 13)])
+        attrText.append(NSMutableAttributedString(string: "      .",
+                                                  attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 2)]))
+
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 120, height: textField.frame.size.height - 10))
+        label.textAlignment = .right
+        label.textColor = .red
+        label.attributedText = attrText
+
+        textField.rightView = label
     }
 
 }
